cc4cca3d76489a3c079cc5a54607cf2c4c89cbdf
Hess-y <hessy683@gmail.com>
2022-11-23 10:44:38 -0800

    task update

--- main.h
+++ main.h
@@ -75,7 +75,6 @@ int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags
 
 int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-<<<<<<< HEAD
 
 
 int print_string(va_list types, char buffer[],
@@ -136,11 +135,6 @@ int write_pointer(char buffer[], int ind, int length,
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
-=======
-int print_string(va_list types, char buffer[], int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start),
-int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size));
-int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
->>>>>>> 261b66e6b422a20104aa12f29efdf98e91dcf57e
 
 /****************** UTILS ******************/
 int is_printable(char);
@@ -150,13 +144,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-<<<<<<< HEAD
-#endif /* MAIN_H */
-=======
-/*Functions to handle other specifiers*/
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
 #endif
-
